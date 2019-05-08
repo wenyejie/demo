75b274bce4462fd70c9e8c23fc2fec3cd8bb0abd
wenyejie <wenyejie@swipal.com>
2019-05-08 18:14:01 +0800

    调整配置
    Signed-off-by: wenyejie <wenyejie@swipal.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
@@ -10,7 +13,7 @@ import Vue from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default Vue.extend({
-  name: 'home',
+  name: 'Home',
   components: {
     HelloWorld
   }
