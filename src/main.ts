34223993a66fc3afb20e959bf53c77faabbe2e5c
wenyejie <wenyejie@swipal.com>
2019-05-09 12:20:27 +0800

    调整store文件结构
    Signed-off-by: wenyejie <wenyejie@swipal.com>

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routers'
-import store from './store'
+import store from './stores'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
