58fdb2800b8f526ee5499e10352cd958fcdd87da
wenyejie <wenyejie@swipal.com>
2019-05-10 08:57:18 +0800

    新增时间数据集
    Signed-off-by: wenyejie <wenyejie@swipal.com>

---
+++ src/dates/months.js
@@ -0,0 +1,11 @@
+/**
+ * @author: Storm
+ * @date: 2019/5/9
+ * @email: wenyejie@foxmail.com
+ */
+
+export const cn: string[] = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二']
+
+export const en: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+export const ens: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'September', 'Sep', 'October', 'November', 'December']
