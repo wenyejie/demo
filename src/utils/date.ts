8454fc6d5e1efbdf1b21b89801e80ea00222115a
wenyejie <wenyejie@swipal.com>
2019-05-10 08:58:13 +0800

    新增时间转换工具
    Signed-off-by: wenyejie <wenyejie@swipal.com>

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,6 +3,7 @@
  * @date: 2019/5/8
  * @email: wenyejie@foxmail.com
  */
+import { isDate, isNumber, zeroize } from '@/utils/common'
 
 export const monthLength = (date: Date = new Date()): number => {
   const val: Date = new Date(date)
@@ -20,4 +21,45 @@ export const isOneMonth = (date1: Date, date2: Date = new Date()): boolean => {
 
 export const isOneYear = (date1: Date, date2: Date = new Date()): boolean => {
   return date1.getFullYear() === date2.getFullYear()
+}
+
+// 转换成date类型
+export const convert = (date: Date | string | number, defVal: any = ''): Date | string => {
+  if (isDate(date)) {
+    return <Date>date
+  }
+
+  if (!date) {
+    return defVal
+  }
+
+  if (typeof date === 'string' && /^\d+$/.test(<string>date)) {
+    date = Number.parseInt(<string>date)
+  }
+
+  if (isNumber(date)) {
+    if ((date + '').length > 13) {
+      date = parseInt((date + '').substr(0, 13))
+    }
+
+    // NaN
+    if (Number.isNaN(<number>date)) {
+      return defVal
+    }
+
+    const length: number = (date + '').length
+
+    if (length < 13) {
+      date = zeroize(<number>date, 13, {
+        pos: 'after',
+        rtnType: 'number'
+      })
+    }
+
+    date = new Date(date)
+  } else {
+    return defVal
+  }
+
+  return <Date>date
 }
